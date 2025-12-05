af58992353d08397130c236e4b150fef5a3a4c96
William <william@wwise.dev>
2025-12-05 01:01:11 -0600

    Update mod details & add suggested mods

--- src/app/convoy/page.tsx
+++ src/app/convoy/page.tsx
@@ -1,21 +1,21 @@
-import styles from './styles.module.scss'
+import styles from './styles.module.scss';
 import type { Metadata, Viewport } from 'next';
 import UpdateGuide from '@/app/convoy/updateGuide';
 
 export const metadata: Metadata = {
-    title: "Hermelinen Convoy",
-    description: "Read up on how to download and sort the mods required to join Quantum Science's ATS convoy."
-}
+    title: 'Hermelinen Convoy',
+    description: 'Read up on how to download and sort the mods required to join Quantum Science\'s ATS convoy.',
+};
 
 export const viewport: Viewport = {
-    themeColor: '#4b93fd'
-}
+    themeColor: '#4b93fd',
+};
 
 export default function ConvoyDetails() {
     return <main className={styles.main}>
         <article>
             <p>
-                <a href='https://realsgii2.dev' target={"_blank"} className={styles.realsgHeader}>RealSGII2/</a>
+                <a href='https://realsgii2.dev' target={'_blank'} className={styles.realsgHeader}>RealSGII2/</a>
             </p>
             <h1>Hermelinen Convoy</h1>
 
@@ -35,35 +35,74 @@ export default function ConvoyDetails() {
             <h2>Notices</h2>
 
             <div className={styles.errorCallout}>
-                <p><b>An optional mod is currently broken.</b></p>
-                <p>The <strong>Better Raindrops</strong> mod will crash your game. The mod author is aware and working on an update.</p>
-            </div>
-
-            <div className={styles.infoCallout}>
-                <p><b>Download links have changed.</b></p>
-                <p>The files have been moved to a new server and downloads should be significantly faster.</p>
+                <p>
+                    <b>Required mods were removed.</b>
+                </p>
+                <p>Check the update notes above for changes.</p>
             </div>
 
             <h2>Download Mods</h2>
             <p>First, subscribe to the steam collection:</p>
-            <a href='https://steamcommunity.com/sharedfiles/filedetails/?id=3577280676' rel={"noreferrer"} target={"_blank"} className={styles.modItem}>
+            <a
+                href='https://steamcommunity.com/sharedfiles/filedetails/?id=3577280676'
+                rel={'noreferrer'}
+                target={'_blank'}
+                className={styles.modItem}
+            >
                 <span>Latest Hermelinen Convoy Pack</span>
                 <span>https://steamcommunity.com/sharedfiles/filedetails/?id=3577280676</span>
             </a>
 
-            <p style={{ marginTop: 16 }}>Next, download the three non-workshop mods:</p>
-            <a href='https://cdn.realsgii2.dev/ats/Edison_BDE.scs' target={"_blank"} className={styles.modItem}>
+            <p style={{ marginTop: 16 }}>Next, download the three required non-workshop mods and optionally any
+                addons:</p>
+            <a href='https://cdn.realsgii2.dev/ats/Edison_BDE.scs' target={'_blank'} className={styles.modItem}>
                 <span>Edison BDE</span>
                 <span>https://cdn.realsgii2.dev/ats/Edison_BDE.scs</span>
             </a>
-            <a href='https://cdn.realsgii2.dev/ats/Dom379.scs' target={"_blank"} className={styles.modItem}>
+            <a href='https://cdn.realsgii2.dev/ats/Dom379.scs' target={'_blank'} className={styles.modItem}>
                 <span>Dom 379</span>
                 <span>https://cdn.realsgii2.dev/ats/Dom379.scs</span>
             </a>
-            <a href='https://cdn.realsgii2.dev/ats/RSG_Megapack.scs' target={"_blank"} className={styles.modItem}>
+            <a href='https://cdn.realsgii2.dev/ats/RSG_Megapack.scs' target={'_blank'} className={styles.modItem}>
                 <span>RealSGII2 Megapack</span>
                 <span>https://cdn.realsgii2.dev/ats/RSG_Megapack.scs</span>
             </a>
+
+            <details>
+                <summary>View optional mods</summary>
+
+                <a
+                    href='https://steamcommunity.com/sharedfiles/filedetails/?id=3596006846'
+                    target={'_blank'}
+                    className={styles.modItem}
+                >
+                    <span>XTX MIRRORS FOV V1.0</span>
+                    <span>Makes your truck&apos;s mirrors have an accurate FOV.</span>
+                </a>
+
+                <a
+                    href='https://steamcommunity.com/sharedfiles/filedetails/?id=3370862855'
+                    target={'_blank'}
+                    className={styles.modItem}
+                >
+                    <span>Realistic Weather System ATS</span>
+                    <span>Changes the weather system to mimic real life areas and makes the intensity range much larger.<br /><i>Recommended to use with &quot;Better Raindrops&quot;!</i></span>
+                </a>
+
+                <a
+                    href='https://steamcommunity.com/sharedfiles/filedetails/?id=2980935675'
+                    target={'_blank'}
+                    className={styles.modItem}
+                >
+                    <span>Better Raindrops</span>
+                    <span>Makes rain drops smaller and more plentiful.<br /><i>Recommended to use with &quot;Realistic Weather System ATS&quot;!</i></span>
+                </a>
+
+                <a href='https://cdn.realsgii2.dev/ats/GoogleMaps.scs' target={'_blank'} className={styles.modItem}>
+                    <span>Google Maps (Dark Theme) (Opt.)</span>
+                    <span>Changes the map to be themed like Google Maps dark.</span>
+                </a>
+            </details>
 
             <p style={{ marginTop: 16 }}>Finally, move these into your mods folder:</p>
             <ul>
@@ -72,7 +111,9 @@ export default function ConvoyDetails() {
             </ul>
 
             <h2>Mod Order</h2>
-            <p>Make sure these mods are in the order listed below. If you want to include your own mods, put them above or below this list. These mods <b>must</b> be next to each other in this order.</p>
+            <p>Make sure these mods are in the order listed below. If you want to include your own mods, put them above
+                or below this list. These mods <b>must</b> be next to each other in this order.
+            </p>
 
             <ol>
                 <li>Real companies, gas stations & billboards</li>
@@ -89,8 +130,8 @@ export default function ConvoyDetails() {
                 <li>Realistic Vehicle Lights Mod v7.4 (by Frkn64)</li>
                 <li>Real Tires Mod</li>
                 <li>SiSL&apos;s Mega Pack</li>
-                <li>Volvo VNL 2018 Improved Dashboard</li>
-                <li>(Closed) Google Maps Navigation Night Version</li>
+                {/*<li>Volvo VNL 2018 Improved Dashboard</li>*/}
+                {/*<li>(Closed) Google Maps Navigation Night Version</li>*/}
                 <li>RealSGII2 Megapack</li>
             </ol>
 
@@ -99,9 +140,15 @@ export default function ConvoyDetails() {
                 <li>Make sure you are at least <b>Level 1</b>. If you aren&apos;t, do jobs until you reach it.</li>
                 <li>Hover over <strong>Convoys</strong> in the garage menu.</li>
                 <li>Click <strong>Convoy lobby</strong>.</li>
-                <li>Search the server ID (&quot;<code>85568392935210120</code>&quot;).</li>
+                <li>Search the server ID (&quot;
+                    <code>85568392935210120</code>
+                    &quot;).
+                </li>
                 <li>Join the server named <strong>Hermelinen Convoy</strong>.</li>
-                <li>Enter the password (&quot;<code>jamesisgay</code>&quot;).</li>
+                <li>Enter the password (&quot;
+                    <code>jamesisgay</code>
+                    &quot;).
+                </li>
             </ol>
 
             <h2>Additional Notes</h2>
@@ -109,18 +156,22 @@ export default function ConvoyDetails() {
                 <li>The server will report a player limit of 8 players. This is incorrect; it is actually 16.</li>
                 <li>The only rule is to not troll other users unless both parties agree to it.</li>
                 <li>Forward complains and suggestions (like mods) to <strong>@SGII2</strong> in QSP.</li>
-                <li>We have a thread in corp-random called <strong>Commercial Vehicle Fan Club</strong>, come join us!</li>
+                <li>We have a thread in corp-random called <strong>Commercial Vehicle Fan Club</strong>, come join us!
+                </li>
             </ul>
 
-            <p className={styles.copyright}>&copy; {new Date().getFullYear()} &mdash; Website built and server maintained by <a className={styles.alink}
-                href='https://realsgii2.dev/'
-                target={"_blank"}
-                rel={"noreferrer"}
-            >RealSGII2</a> &mdash; <a className={styles.alink}
-                href='https://github.com/RealSGII2/hermelinen-rickroll'
-                target={"_blank"}
-                rel={"noreferrer"}
-            >View website source</a></p>
+            <p className={styles.copyright}>&copy; {new Date().getFullYear()} &mdash; Website built and server
+                maintained by <a
+                    className={styles.alink} href='https://realsgii2.dev/' target={'_blank'} rel={'noreferrer'}
+                >RealSGII2</a> &mdash;
+                <a
+                    className={styles.alink}
+                    href='https://github.com/RealSGII2/hermelinen-rickroll'
+                    target={'_blank'}
+                    rel={'noreferrer'}
+                >View website source
+                </a>
+            </p>
         </article>
-    </main>
+    </main>;
 }
--- src/app/convoy/updateGuide.tsx
+++ src/app/convoy/updateGuide.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import styles from './styles.module.scss'
+import styles from './styles.module.scss';
 import { useState } from 'react';
 
 export default function UpdateGuide() {
@@ -9,37 +9,40 @@ export default function UpdateGuide() {
     return <>
         <div className={`${styles.updateGuide}${shown ? ' ' + styles.shown : ''}`}>
             <button onClick={() => setShown(!shown)}>
-                <p><b>Need to update mods?</b></p>
+                <p>
+                    <b>Need to update mods?</b>
+                </p>
                 <div>{shown ? 'Close' : 'View update guide'}</div>
             </button>
 
             {shown && <div>
-                <h2>Updated Mods</h2>
-                <p>Download these and replace the old ones in your mods folder.</p>
+                <h2>Removed Mods</h2>
+                <p>The following mods were <b>removed</b> and cannot be used on the server anymore:</p>
+                <ul>
+                    <li>Google Maps Navigation Night Theme</li>
+                    <li>Volvo VNL Improved Dashboard</li>
+                </ul>
 
-                <a href='https://cdn.realsgii2.dev/ats/Dom379.scs' target={"_blank"} className={styles.modItem}>
-                    <span>Dom 379</span>
-                    <span>https://cdn.realsgii2.dev/ats/Dom379.scs</span>
+                <p style={{ marginTop: 16 }}>These mods introduced client only changes and were required by the server.
+                    To continue the philosophy of not requiring client mods, they were removed.</p>
+
+                <p style={{ marginTop: 16 }}>If you wish to continue using the Google Maps mod, you can download the
+                    optional version:</p>
+
+                <a href='https://cdn.realsgii2.dev/ats/GoogleMaps.scs' target={'_blank'} className={styles.modItem}>
+                    <span>Google Maps (Dark Theme) (Opt.)</span>
+                    <span>https://cdn.realsgii2.dev/ats/GoogleMaps.scs</span>
                     <ul>
-                        <li>Update to support 1.57.x.</li>
-                        <li>Merge with Dom lighting mod.</li>
-                    </ul>
-                </a>
-                <a href='https://cdn.realsgii2.dev/ats/RSG_Megapack.scs' target={"_blank"} className={styles.modItem}>
-                    <span>RealSGII2 Megapack</span>
-                    <span>https://cdn.realsgii2.dev/ats/RSG_Megapack.scs</span>
-                    <ul>
-                        <li>Remove buggy physics mod.</li>
-                        <li>Make mudflaps opaque.</li>
+                        <li>Mark as optional</li>
                     </ul>
                 </a>
 
                 <h2>Notes</h2>
                 <ul>
                     <li>The mod order did not change this update.</li>
-                    <li>dom_379_156.scs was renamed to Dom379.scs; you&apos;d want to delete the old file.</li>
+                    <li>The total mod count is now 15.</li>
                 </ul>
             </div>}
         </div>
-    </>
+    </>;
 }
